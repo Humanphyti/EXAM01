de48f325f2fe1dc8fa3c97fe3a459f4118612f1d
Humanphyti <humanphyti@gmail.com>
2016-10-06 21:33:52 -0700

    comments amended

--- EXAM_01/EXAM_01/Movie.cpp
+++ EXAM_01/EXAM_01/Movie.cpp
@@ -1,8 +1,9 @@
 #include "Movie.h"
-
+//standard constructor with a genre check
 Movie::Movie(string newTitle, string newGenre, int newShowtime)
 {
 	title = newTitle;
+	//output the genre name if it falls in the categories or default to comedy
     if(genre == "Action" || genre == "Horror" || genre == "Documentary" || genre == "Comedy")
         genre = newGenre;
     else
