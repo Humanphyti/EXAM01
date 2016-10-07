8d6204cfffec638b513312017de1c073a032eef2
Humanphyti <humanphyti@gmail.com>
2016-10-06 21:32:39 -0700

    comments amended

--- EXAM_01/EXAM_01/Movie.h
+++ EXAM_01/EXAM_01/Movie.h
@@ -7,21 +7,21 @@ using namespace std;
 class Movie {
 
 public:
+	// constuctor
 	Movie(string newTitle, string newGenre, int newShowtime);
 
 	string getTitle();
-
+	
 	string getGenre();
 
 	int getShowtime();
 
 private:
+	//need to remain protected and only editable via functions
 	string title;
 
 	string genre;
 
-	int showtime; //must be in 24 hour time.
-
-    int* Movies;
+	int showtime; // 24 hours in a day so you better be entering a value between 0 and 23
 
 };
