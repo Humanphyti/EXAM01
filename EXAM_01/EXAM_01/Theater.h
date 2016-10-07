51d727974c1b1722bfd0c2761d1f8cb07b4060be
Humanphyti <humanphyti@gmail.com>
2016-10-06 21:24:38 -0700

    comments amended

--- EXAM_01/EXAM_01/Theater.h
+++ EXAM_01/EXAM_01/Theater.h
@@ -1,13 +1,3 @@
-//
-//  Theater.h
-//  Exam 1
-//
-//  Created by AJ Cendejas on 5/29/14.
-//  Copyright (c) 2014 AJ Cendejas. All rights reserved.
-//  I affirm that all code given below was written solely by me,
-//  AJ Cendejas, and that any help I received adhered to the
-//  rules stated for this exam.
-//
 
 #ifndef __Exam_1__Theater__
 #define __Exam_1__Theater__
@@ -17,21 +7,13 @@
 
 using namespace std;
 
-//Realistically there probably wouldn't be a movie every hour in a single showing theater but why not.
-const int HOURS = 24;
+// set static constatnt for array
+static const int HOURS = 24;
 
 class Theater
 {
-private:
-    //Properties (Public shouldn't be able to change Name, Phone, or Prices of amenities in a Theater)
-    string Name;
-    string Phone;
-    Movie MovieList[HOURS];
-    int PricePopcorn; //Decided to make the prices properties of the theater so the functions could reference something
-    int PriceSoda; //Most of the time in a theater you buy a drink and decide after what kind.
-    
 public:
-    //Functions
+    // Theater Functions
     void AddMovie(Movie& Movie);
     Movie GetMovieList(int i);
     string GetMovieForHour(int hour);
@@ -39,8 +21,18 @@ public:
     int GetPopcornPrice();
     int GetCokePrice();
     
-    //Constructor
-    Theater(string name, string phone);
+    // Theater Constructor
+    Theater(string name, string phone);    
+   
+private:
+    // Values of theater. necessarily untouchable by public.
+    string Name;
+    string Phone;
+    Movie MovieList[HOURS];
+    int PricePopcorn; 
+    int PriceSoda;
+    
+
     
 };
 
